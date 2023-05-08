fa95fb87121a4d24c0ff8bd36608a8725e626f14
byDsh116 <by.dsh116@gmail.com>
2023-05-08 17:07:27 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AppHeader from './appHeader/AppHeader';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="body">
+        <AppHeader />
+      </div>
+    </div>
+  );
+}
+
+export default App;
