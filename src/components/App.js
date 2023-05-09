8f6b7215c27e4edad8e38557e5cb7d5f677c6147
byDsh116 <by.dsh116@gmail.com>
2023-05-09 10:31:11 +0300

    add plug img for random char

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import AppHeader from './appHeader/AppHeader';
+import RandomChar from './randomChar/RandomChar';
 
+
+let plugImg = <img src="https://i.pinimg.com/564x/b9/11/db/b911dbd417c2eac3fa7b02db2da7446a.jpg" alt="" />
 
 
 function App() {
@@ -8,6 +11,12 @@ function App() {
     <div className="App">
       <div className="body">
         <AppHeader />
+        <main>
+
+
+          <RandomChar img={plugImg} />
+
+        </main>
       </div>
     </div>
   );
---
+++ src/components/randomChar/RandomChar.js
@@ -0,0 +1,14 @@
+
+
+const RandomChar = (props) => {
+    return (
+        <div className="randomchar">
+            <div className="randomchar__img">
+                this is   {props.img}
+            </div>
+
+        </div>
+    )
+}
+
+export default RandomChar
