a622a1722f3bc41a33862aaba7bea5e76f604176
byDsh116 <by.dsh116@gmail.com>
2023-05-10 13:26:57 +0300

    add char descr, try to do style for button

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import AppHeader from './appHeader/AppHeader';
 import RandomChar from './randomChar/RandomChar';
 
 
-let plugImg = <img src="https://i.pinimg.com/564x/b9/11/db/b911dbd417c2eac3fa7b02db2da7446a.jpg" alt="" />
+let plugImg = <img className='randomchar__img' src="https://i.pinimg.com/564x/b9/11/db/b911dbd417c2eac3fa7b02db2da7446a.jpg" alt="Random character" />
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <main>
 
 
-          <RandomChar img={plugImg} />
+          <RandomChar img={plugImg} name={'NAME'} />
 
         </main>
       </div>
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,12 +1,26 @@
+import './randomChar.scss'
+
 
 
 const RandomChar = (props) => {
     return (
-        <div className="randomchar">
-            <div className="randomchar__img">
-                this is   {props.img}
+        <div className='randomchar'>
+            <div className='randomchar__block'>
+                {props.img}
+                <div className='randomchar__info'>
+                    <p className="randomchar__name">{props.name}</p>
+                    <p className='randomchar__descr'>
+                        As the Norse God of thunder and lightning,
+                        Thor wields one of the greatest weapons ever made,
+                        the enchanted hammer Mjolnir. While others have described Thor as an over-muscled,
+                        oafish imbecile, he's quite smart and compassionate...
+                    </p>
+                    <div className='randomchar__btn'>
+                        <button className='button__red'>HOMEPAGE</button>
+                        <button className='button__grey'>WIKI</button>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
