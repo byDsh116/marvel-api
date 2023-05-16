53fbd11802e41906632fa87055e33570c22c08cc
byDsh116 <by.dsh116@gmail.com>
2023-05-16 16:31:04 +0300

    Improve randomchar block

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,15 +1,14 @@
 import Button from '../button/Button'
 import './randomChar.scss'
 import mjolnir from '../../resources/img/mjolnir.png'
-
-
+import thor from '../../resources/img/thor.jpeg'
 
 
 const RandomChar = (props) => {
     return (
         <div className='randomchar'>
             <div className='randomchar__block'>
-                {props.img}
+                <img src={thor} className='randomchar__img' />
                 <div className='randomchar__info'>
                     <p className="randomchar__name">{props.name}</p>
                     <p className='randomchar__descr'>
@@ -18,18 +17,20 @@ const RandomChar = (props) => {
                         the enchanted hammer Mjolnir. While others have described Thor as an over-muscled,
                         oafish imbecile, he's quite smart and compassionate...
                     </p>
-
                     <div className='randomchar__btns'>
                         <Button />
                         <Button />
-
                     </div>
                 </div>
             </div>
-
             <div className='randomchar__choose-block'>
-                <p className='randomchar__title'>Random character for today !</p>
-                <p className='randomchar__title'>Do you want to get to know him better?</p>
+                <p className='randomchar__title'>
+                    Random character for today !<br />
+                    Do you want to get to know him better?
+                </p>
+                <p className='randomchar__title'>
+                    Or choose another one
+                </p>
                 <Button />
                 <img src={mjolnir} alt="" className='randomchar__decoration' />
             </div>
