5f4434fc9a8c73698270709fe6ee6600b0923e68
byDsh116 <by.dsh116@gmail.com>
2023-05-29 22:09:31 +0300

    add char cards component into app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import '../../style/style.scss'
 import AppHeader from '../appHeader/AppHeader';
 import RandomChar from '../randomChar/RandomChar';
 import CharInfo from '../charinfo/CharInfo';
+import CharCards from '../charCards/CharCards'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <RandomChar name={'NAME'} />
         <div className='char__info'>
           <CharInfo />
+          <CharCards />
         </div>
       </main>
     </div>
