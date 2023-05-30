5b2d4a47b7cad513003e2e6b8e3cd09895523eb0
byDsh116 <by.dsh116@gmail.com>
2023-05-30 23:16:23 +0300

    structure edited: removed extra <p> in charinfo name, and changed classes. Add styles

--- src/components/charinfo/CharInfo.js
+++ src/components/charinfo/CharInfo.js
@@ -7,7 +7,7 @@ const CharInfo = () => {
         <div className='charinfo'>
             <div className='charinfo__descr'>
                 <img src={loki} alt='' />
-                <div><p className='charinfo__name'>loki</p></div>
+                <div className='charinfo__name'>loki</div>
                 <div className='charinfo__btns'>
                     <button className='button button__main'>
                         <div className='inner'>
@@ -20,30 +20,54 @@ const CharInfo = () => {
                         </div>
                     </button>
                 </div>
-                <div>
-                    <p className='charinfo__descr-text'> In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.</p>
+                <div className='charinfo__descr-text'>
+                    <p >
+                        In Norse mythology, Loki is a god or jötunn (or both).
+                        Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr.
+                        By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir,
+                        and the world serpent Jörmungandr.
+                        By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion
+                        Svaðilfari as the father, Loki gave birth—in the form of a mare—to the
+                        eight-legged horse Sleipnir. In addition, Loki is referred to as the father
+                        of Váli in the Prose Edda.
+                    </p>
                 </div>
                 <div>
-                    <p className='charinfo__comics-block'>
+                    <p className='charinfo__comics'>
                         Comics:
                     </p>
                 </div>
-                <ul>
-                    <li>
+                <ul className='charinfo__comics-list'>
+                    <li className='charinfo__comics-item'>
                         All-Winners Squad: Band of Heroes (2011) #3
                     </li>
-                    <li>
+                    <li className='charinfo__comics-item'>
                         Alpha Flight (1983) #50
                     </li>
-                    <li>
+                    <li className='charinfo__comics-item'>
                         Amazing Spider-Man (1999) #503
                     </li>
-                    <li>
-
+                    <li className='charinfo__comics-item'>
+                        Amazing Spider-Man (1999) #504
                     </li>
+                    <li className='charinfo__comics-item'>
+                        AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
+                    </li>
+                    <li className='charinfo__comics-item'>
+                        Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
+                    </li>
+                    <li className='charinfo__comics-item'>
+                        Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
+                    </li>
+                    <li className='charinfo__comics-item'>
+                        Vengeance (2011) #4                    </li>
+                    <li className='charinfo__comics-item'>
+                        Avengers (1963) #1                    </li>
+                    <li className='charinfo__comics-item'>
+                        Avengers (1996) #1                    </li>
                 </ul>
             </div>
-        </div>
+        </div >
     )
 }
 
